05b3a07079e349fe643b3e2abf9db30704454702
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-10-17 11:50:08 -0500

    adding express

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req,res ) => {
-  res.end('Hello world')
-})
+const app = express()
+app.get('/', (req, res) => res.send('Hello world with express'))
 
-server.listen(3000)
+app.listen(3000)
 console.log('Server running on port 3000')

876f24aa807284a7595a3bd710236b13de94bd3c
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-10-17 11:43:32 -0500

    uploading to AWS

---
+++ connecting to aws through ssh.ts
@@ -0,0 +1,15 @@
+/* 
+  --open a windows console
+  --verify ssh on windows
+    ssh -v
+
+  --to connect remote server throught ssh
+    ssh ubuntu@18.216.31.146 -i app-hotel.pem
+       ♥ be default the server has root use, but we will use ubuntu
+
+*/
+
+/* 
+  --to update repositories and programs
+    sudo apt update && sudo apt upgrade
+*/
